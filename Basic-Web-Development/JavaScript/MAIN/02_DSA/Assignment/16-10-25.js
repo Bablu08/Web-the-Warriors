a4ab671c225b4a6533034357512be3d46d84f7ae
Bablu08 <rajkishorebehera358@gmail.com>
2025-10-16 17:16:44 +0530

    update array assignment

--- Basic-Web-Development/JavaScript/MAIN/02_DSA/Assignment/16-10-25.js
+++ Basic-Web-Development/JavaScript/MAIN/02_DSA/Assignment/16-10-25.js
@@ -1,20 +1,56 @@
 // Print alternative elements
-let arr = [48, 12, 66, 68, 0o1]
-// output = 48, 66, 0o1
-let index = 0
-for(let num of arr){
-    if(index % 2 == 0){
-        console.log(num)
-    }
-    index++
-}
+// let arr = [48, 12, 66, 68, 0o1]
+// // output = 48, 66, 0o1
+// let index = 0
+// for(let num of arr){
+//     if(index % 2 == 0){
+//         console.log(num)
+//     }
+//     index++
+// }
 
 // LINEAR SEARCH
-arr = [15, 25, 26, 45, 66, 78, 11]
+let arr = [15, 25, 26, 45, 66, 78, 11]
 // find: 78, 10
-index = 0
-for(num of arr){
-    if(num){
-        
+
+let key = 78
+let found = false
+
+for(let num in arr){
+    if(arr[num] === key){
+        console.log(`Element ${key} found at index ${num}`)
+        found = true
+        break
     }
+}
+
+if (!found) {
+  console.log(`Element ${key} not found`);
+}
+
+// FIND THE GRATEST ELEMENT (4891)
+arr = [10, 20, 4, 556, 4891, 1, 12, 46]
+
+let hold = arr[0]
+var ptr
+for(ptr of arr){
+    if(ptr > hold){
+        hold = ptr
+    }
+}
+console.log(hold)
+
+// FIND THE SMALLEST ELEMENT (1)
+hold = arr[0]
+for(ptr of arr){
+    if(ptr < hold){
+        hold = ptr
+    }
+}
+console.log(hold)
+
+// REVERSE THE ARRAY
+// let reversed = []
+for(ptr in arr.reverse()){
+    console.log(`index ${ptr} contain ${arr[ptr]}`)
 }
