1b344e330a8059abcfa8489307fd4c099c1616d9
Bablu08 <rajkishorebehera358@gmail.com>
2025-10-17 16:14:23 +0530

    Update

---
+++ Basic-Web-Development/JavaScript/MAIN/02_DSA/Code/02_Strings/data.js
@@ -0,0 +1,21 @@
+// # ← contains master string
+//* =========================================
+//* String in JavaScript
+//* =========================================
+
+//? Strings in JavaScript are a fundamental data type that represents a sequence of characters.
+
+//* String Properties:
+//? length: Property that returns the length of the string (number of characters).
+
+let str = "Hello World!"
+console.log(str.length)
+
+//* =========================================
+//* Escape Character
+//* =========================================
+
+//? Escape Character: In JavaScript, the backslash \ is used as an escape character. It allows you to include special characters in a string.
+
+let text = "My name is \"Rajkishore\" & I am a full stack developer."
+console.log(text)
