c437afb641546467585792ed14ce60419705c89f
Bablu08 <rajkishorebehera358@gmail.com>
2025-10-17 10:25:08 +0530

    update elements

--- Basic-Web-Development/JavaScript/MAIN/02_DSA/Assignment/16-10-25.js
+++ Basic-Web-Development/JavaScript/MAIN/02_DSA/Assignment/16-10-25.js
@@ -1,16 +1,16 @@
 // Print alternative elements
-// let arr = [48, 12, 66, 68, 0o1]
-// // output = 48, 66, 0o1
-// let index = 0
-// for(let num of arr){
-//     if(index % 2 == 0){
-//         console.log(num)
-//     }
-//     index++
-// }
+let arr = [48, 12, 66, 68, 0o1]
+// output = 48, 66, 0o1
+let index = 0
+for(let num of arr){
+    if(index % 2 == 0){
+        console.log(num)
+    }
+    index++
+}
 
 // LINEAR SEARCH
-let arr = [15, 25, 26, 45, 66, 78, 11]
+arr = [15, 25, 26, 45, 66, 78, 11]
 // find: 78, 10
 
 let key = 78
