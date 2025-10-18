4ae87be5011b5661bda8c4fd3200af97144df471
Bablu08 <rajkishorebehera358@gmail.com>
2025-10-18 16:59:39 +0530

    CURD operation porform ion files

--- Basic-Web-Development/PHP/PROJECT/app/views/home.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>My PHP App</title>
-</head>
-<body>
-    <h1>Welcome to My PHP Project!</h1>
-</body>
-</html>
--- Basic-Web-Development/PHP/PROJECT/public/index.php
+++
@@ -1,6 +0,0 @@
-<?php
-require_once __DIR__ . '/../app/config/config.php';
-require_once __DIR__ . '/../app/controllers/HomeController.php';
-
-$controller = new HomeController();
-$controller->index();
